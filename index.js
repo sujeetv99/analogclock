11e791419e971ed36d3cbb5c086188bf964a2b99
sujeetv99 <90466199+sujeetv99@users.noreply.github.com>
2024-10-07 08:08:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
+    let hrotation = 30 * htime + mtime / 2;
+    let mrotation = 6 * mtime;
+    let srotation = 6 * stime;
+
+    document.getElementById('hour').style.transform = `rotate(${hrotation}deg)`;
+    document.getElementById('minute').style.transform = `rotate(${mrotation}deg)`;
+    document.getElementById('second').style.transform = `rotate(${srotation}deg)`;
+}, 1000);
